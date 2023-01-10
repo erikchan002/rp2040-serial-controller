9cf35a20784ab5ebd98ba1087a771041d597bb9d
erikchan002 <erikchan002@gmail.com>
2023-01-10 17:54:57 +0800

    - Respond with the current gamepad state when any packet is received

--- include/gamepad.h
+++ include/gamepad.h
@@ -10,10 +10,12 @@ class Gamepad : public MPG {
   void read();
   PacketSerial packetSerial;
   CRC8 crc8;
+  static const size_t PACKET_SIZE = 16;
 
   void onPacketReceived(const uint8_t* buffer, size_t size);
   static void onPacketReceived(const void* sender, const uint8_t* buffer,
                                size_t size) {
     ((Gamepad*)sender)->onPacketReceived(buffer, size);
   }
+  void sendStatePacket();
 };
--- src/gamepad.cpp
+++ src/gamepad.cpp
@@ -9,47 +9,80 @@ void Gamepad::setup() {
 void Gamepad::read() { packetSerial.update(); }
 
 void Gamepad::onPacketReceived(const uint8_t* buffer, size_t size) {
+  if (size != PACKET_SIZE) {
+    sendStatePacket();
+    return;
+  }
   crc8.restart();
   crc8.add(buffer, size);
   if (crc8.getCRC() != 0) {
+    sendStatePacket();
     return;
   }
 
   size_t index = 0;
 
-  if (index + sizeof(state.dpad) > size) return;
   memcpy(&state.dpad, buffer + index, sizeof(state.dpad));
   index += sizeof(state.dpad);
 
-  if (index + sizeof(state.buttons) > size) return;
   memcpy(&state.buttons, buffer + index, sizeof(state.buttons));
   index += sizeof(state.buttons);
 
-  if (index + sizeof(state.aux) > size) return;
   memcpy(&state.aux, buffer + index, sizeof(state.aux));
   index += sizeof(state.aux);
 
-  if (index + sizeof(state.lx) > size) return;
   memcpy(&state.lx, buffer + index, sizeof(state.lx));
   index += sizeof(state.lx);
 
-  if (index + sizeof(state.ly) > size) return;
   memcpy(&state.ly, buffer + index, sizeof(state.ly));
   index += sizeof(state.ly);
 
-  if (index + sizeof(state.rx) > size) return;
   memcpy(&state.rx, buffer + index, sizeof(state.rx));
   index += sizeof(state.rx);
 
-  if (index + sizeof(state.ry) > size) return;
   memcpy(&state.ry, buffer + index, sizeof(state.ry));
   index += sizeof(state.ry);
 
-  if (index + sizeof(state.lt) > size) return;
   memcpy(&state.lt, buffer + index, sizeof(state.lt));
   index += sizeof(state.lt);
 
-  if (index + sizeof(state.rt) > size) return;
   memcpy(&state.rt, buffer + index, sizeof(state.rt));
   index += sizeof(state.rt);
+
+  sendStatePacket();
+}
+
+void Gamepad::sendStatePacket() {
+  uint8_t buffer[PACKET_SIZE];
+
+  size_t index = 0;
+
+  memcpy(buffer + index, &state.dpad, sizeof(state.dpad));
+  index += sizeof(state.dpad);
+
+  memcpy(buffer + index, &state.buttons, sizeof(state.buttons));
+  index += sizeof(state.buttons);
+
+  memcpy(buffer + index, &state.aux, sizeof(state.aux));
+  index += sizeof(state.aux);
+
+  memcpy(buffer + index, &state.lx, sizeof(state.lx));
+  index += sizeof(state.lx);
+
+  memcpy(buffer + index, &state.ly, sizeof(state.ly));
+  index += sizeof(state.ly);
+
+  memcpy(buffer + index, &state.rx, sizeof(state.rx));
+  index += sizeof(state.rx);
+
+  memcpy(buffer + index, &state.ry, sizeof(state.ry));
+  index += sizeof(state.ry);
+
+  memcpy(buffer + index, &state.lt, sizeof(state.lt));
+  index += sizeof(state.lt);
+
+  memcpy(buffer + index, &state.rt, sizeof(state.rt));
+  index += sizeof(state.rt);
+
+  packetSerial.send(buffer, PACKET_SIZE);
 }
